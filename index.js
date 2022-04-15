869d7feb045ce678c19a53f246b6300cada8f7c9
renato3x <pereirarenato21@gmail.com>
2022-04-15 10:06:35 -0300

    audio removed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function blink(typing) {
-
   return setInterval(() => {
     typing.style.borderRightColor = 'transparent'
 
@@ -14,24 +13,22 @@ function stopBlink(id, typing) {
   typing.style.borderRightColor = 'var(--cursor-color)'
 }
 
-async function startTyping(typing, typingContent, typingWidth, quantityToRemoveOfTyping, blinkId, audio) {
+async function startTyping(typing, typingContent, typingWidth, quantityToRemoveOfTyping, blinkId) {
   typing.style.width = '0'
 
   let i = typingContent.textContent.length
 
-  blinkId = await type(typing, typingWidth, quantityToRemoveOfTyping, i, blinkId, audio)
-  blinkId = await unType(typing, typingWidth, quantityToRemoveOfTyping, i, blinkId, audio)
+  blinkId = await type(typing, typingWidth, quantityToRemoveOfTyping, i, blinkId)
+  blinkId = await unType(typing, typingWidth, quantityToRemoveOfTyping, i, blinkId)
 
-  startTyping(typing, typingContent, typingWidth, quantityToRemoveOfTyping, blinkId, audio)
+  startTyping(typing, typingContent, typingWidth, quantityToRemoveOfTyping, blinkId)
 }
 
-function type(typing, typingWidth, quantityToRemoveOfTyping, typingContentLength, blinkId, audio) {
+function type(typing, typingWidth, quantityToRemoveOfTyping, typingContentLength, blinkId) {
   return new Promise((resolve, _) => {
     let i = typingContentLength
 
     setTimeout(() => {
-      audio.play()
-
       stopBlink(blinkId, typing)
 
       let id = setInterval(() => {
@@ -41,7 +38,6 @@ function type(typing, typingWidth, quantityToRemoveOfTyping, typingContentLength
     
         if (i == -1) {
           clearInterval(id)
-          audio.pause()
           resolve(blink(typing))
         }
       }, 100)
@@ -49,12 +45,11 @@ function type(typing, typingWidth, quantityToRemoveOfTyping, typingContentLength
   })
 }
 
-function unType(typing, typingWidth, quantityToRemoveOfTyping, typingContentLength, blinkId, audio) {
+function unType(typing, typingWidth, quantityToRemoveOfTyping, typingContentLength, blinkId) {
   return new Promise((resolve, _) => {
     let i = 1
 
     setTimeout(() => {
-      audio.play()
       stopBlink(blinkId, typing)
 
       let id = setInterval(() => {
@@ -64,7 +59,6 @@ function unType(typing, typingWidth, quantityToRemoveOfTyping, typingContentLeng
     
         if (i > typingContentLength) {
           clearInterval(id)
-          audio.pause()
           resolve(blink(typing))
         }
       }, 100)
@@ -81,10 +75,6 @@ const typingContentCS = getComputedStyle(typingContent)
 const typingWidth = parseFloat(typingCS.width.replace('px', ''))
 const quantityToRemoveOfTyping = parseFloat(typingContentCS.width.replace('px', '') / typingContent.textContent.length)
 
-const audio = new Audio('typing.mp3')
-audio.load()
-audio.loop = true
-audio.volume = 0.6
-
 let blinkId = blink(typing)
-startTyping(typing, typingContent, typingWidth, quantityToRemoveOfTyping, blinkId, audio)
+
+startTyping(typing, typingContent, typingWidth, quantityToRemoveOfTyping, blinkId)
